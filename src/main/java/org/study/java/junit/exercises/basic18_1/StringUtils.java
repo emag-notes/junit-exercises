4a5745159ef74105e5ae97119943e1288652e2f4
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-11-26 00:33:10 +0900

    toSnakeCaseにJavadocコメントを追加

--- src/main/java/org/study/java/junit/exercises/basic18_1/StringUtils.java
+++ src/main/java/org/study/java/junit/exercises/basic18_1/StringUtils.java
@@ -2,6 +2,11 @@ package org.study.java.junit.exercises.basic18_1;
 
 public class StringUtils {
 
+    /**
+     * 引数の文字列をスネークケース(すべて小文字で単語区切りがアンダースコア)に変換した文字列を返す
+     * @param src 元の文字列
+     * @return スネークケースに変換した文字列
+     */
     public static String toSnakeCase(String src) {
         return "aaa";
     }
