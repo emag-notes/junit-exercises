071f6b967f92e2b503519c03b0a992641230b46e
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-11-25 23:42:21 +0900

    StringUtils#toSnakeCaseの仮実装

--- src/main/java/org/study/java/junit/exercises/basic18_1/StringUtils.java
+++ src/main/java/org/study/java/junit/exercises/basic18_1/StringUtils.java
@@ -3,8 +3,7 @@ package org.study.java.junit.exercises.basic18_1;
 public class StringUtils {
 
     public String toSnakeCase(String string) {
-        // TODO Auto-generated method stub
-        return null;
+        return "aaa";
     }
 
 }
