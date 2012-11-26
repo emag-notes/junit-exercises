fda210d4a8c5f6379ff65dcea17f98b197deb352
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-11-27 00:38:11 +0900

    Calculator#divideの仮引数名をわかりやすくした

--- src/main/java/org/study/java/junit/exercises/basic18_2/Calculator.java
+++ src/main/java/org/study/java/junit/exercises/basic18_2/Calculator.java
@@ -2,11 +2,11 @@ package org.study.java.junit.exercises.basic18_2;
 
 public class Calculator {
 
-    public static int divide(int x, int y) {
-        if (y == 0) {
+    public static int divide(int dividend, int divisor) {
+        if (divisor == 0) {
             throw new IllegalArgumentException("divisor must not be zero!");
         }
-        return x / y;
+        return dividend / divisor;
     }
 
 }
