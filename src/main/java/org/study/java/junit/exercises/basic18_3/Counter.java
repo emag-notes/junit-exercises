da69ddf901680ec9db54785f0f5076a40360a8f7
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-11-27 08:22:47 +0900

    Counter#incrementのフィールドcounterをフィールドとわかる形に修正(末尾に_を追記)

--- src/main/java/org/study/java/junit/exercises/basic18_3/Counter.java
+++ src/main/java/org/study/java/junit/exercises/basic18_3/Counter.java
@@ -2,10 +2,14 @@ package org.study.java.junit.exercises.basic18_3;
 
 public class Counter {
 
-    private int counter;
+    private int counter_;
     
+    /**
+     * 実行するたびに1ずつ加算された値(初回の呼び出しは1)を返す。
+     * @return 現在のカウントから +1 された値
+     */
     public int increment() {
-        return ++counter;
+        return ++counter_;
     }
 
 }
