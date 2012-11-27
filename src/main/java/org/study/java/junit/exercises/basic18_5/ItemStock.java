2502049ed45895a477cbcb0e8219bec701487633
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-11-28 01:12:19 +0900

    ItemStock#getNum,addの本実装

--- src/main/java/org/study/java/junit/exercises/basic18_5/ItemStock.java
+++ src/main/java/org/study/java/junit/exercises/basic18_5/ItemStock.java
@@ -1,9 +1,23 @@
 package org.study.java.junit.exercises.basic18_5;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ItemStock {
 
+    private Map<Item, Integer> cart_ = new HashMap<Item, Integer>();
+    
     public int getNum(Item item) {
-        return 0;
+        Integer itemNum = cart_.get(item);
+        return itemNum != null ? itemNum.intValue() : 0;
+    }
+
+    public void add(Item item) {
+        if (getNum(item) == 0) {
+            cart_.put(item, Integer.valueOf(1));
+        } else {
+            cart_.put(item, getNum(item)+1);
+        }
     }
 
 }
--- src/test/java/org/study/java/junit/exercises/basic18_5/ItemStockTest.java
+++ src/test/java/org/study/java/junit/exercises/basic18_5/ItemStockTest.java
@@ -13,19 +13,27 @@ public class ItemStockTest {
 
     public static class 初期状態 {
         ItemStock sut;
+        Item item;
         
         @Before
         public void setUp() throws Exception {
             sut = new ItemStock();
+            item = new Item("A", 100);
         }
         
         @Test
-        public void getNumは0を() throws Exception {
-            // SetUp
-            Item item = new Item("A", 100);
+        public void getNumは0を返す() throws Exception {
             // Exercise
             // Verify
             assertThat(sut.getNum(item), is(0));
+        }
+        
+        @Test
+        public void addでItemを追加するとgetNumは1を返す() throws Exception {
+            // Exercise
+            sut.add(item);
+            // Verify
+            assertThat(sut.getNum(item), is(1));
         }
         
     }
