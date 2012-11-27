380684b7f5e3b887400012cd453c66f315b098b3
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-11-28 01:52:09 +0900

    ItemStockTestのテストメソッド名を修正

--- src/test/java/org/study/java/junit/exercises/basic18_5/ItemStockTest.java
+++ src/test/java/org/study/java/junit/exercises/basic18_5/ItemStockTest.java
@@ -14,76 +14,76 @@ public class ItemStockTest {
     public static class 初期状態 {
         
         ItemStock sut;
-        Item item;
+        Item itemA100;
         
         @Before
         public void setUp() throws Exception {
             sut = new ItemStock();
-            item = new Item("A", 100);
+            itemA100 = new Item("A", 100);
         }
         
         @Test
-        public void getNumは0を返す() throws Exception {
+        public void getNum_itemA100は0を返す() throws Exception {
             // Exercise
             // Verify
-            assertThat(sut.getNum(item), is(0));
+            assertThat(sut.getNum(itemA100), is(0));
         }
         
         @Test
-        public void addでItemを追加するとgetNumは1を返す() throws Exception {
+        public void add_itemA100の実行後はgetNum_itemA100は1を返す() throws Exception {
             // Exercise
-            sut.add(item);
+            sut.add(itemA100);
             // Verify
-            assertThat(sut.getNum(item), is(1));
+            assertThat(sut.getNum(itemA100), is(1));
         }
         
     }
     
-    public static class Itemが1つ追加された状態 {
+    public static class ItemA100が1つ追加された状態 {
         
         ItemStock sut;
-        Item itemA;
+        Item itemA100;
         
         @Before
         public void setUp() throws Exception {
             sut = new ItemStock();
-            itemA = new Item("A", 100);
-            sut.add(itemA);
+            itemA100 = new Item("A", 100);
+            sut.add(itemA100);
         }
         
         @Test
-        public void getNum_itemAは0を返す() throws Exception {
+        public void getNum_itemA100は1を返す() throws Exception {
             // Exercise
             // Verify
-            assertThat(sut.getNum(itemA), is(1));
+            assertThat(sut.getNum(itemA100), is(1));
         }
         
         @Test
-        public void add_itemAの実行後はgetNum_itemAは2を返す() throws Exception {
+        public void add_itemA100の実行後はgetNum_itemA100は2を返す() throws Exception {
             // Exercise
-            sut.add(itemA);
+            sut.add(itemA100);
             // Verify
-            assertThat(sut.getNum(itemA), is(2));
+            assertThat(sut.getNum(itemA100), is(2));
         }
         
         @Test
-        public void add_itemBの実行後はgetNum_itemBは1を返す() throws Exception {
+        public void add_itemB200の実行後はgetNum_itemB200は1を返す() throws Exception {
             // SetUp
-            Item itemB = new Item("B", 200);
+            Item itemB200 = new Item("B", 200);
             // Exercise
-            sut.add(itemB);
+            sut.add(itemB200);
             // Verify
-            assertThat(sut.getNum(itemB), is(1));
+            assertThat(sut.getNum(itemB200), is(1));
         }
         
         @Test
-        public void nameがAでpriceが200であるitemA200をaddした後はget_itemAは2を返す() throws Exception {
+        public void add_itemA200の実行後はget_itemAは2を返す() throws Exception {
             // SetUp
             Item itemA200 = new Item("A", 200);
             // Exercise
             sut.add(itemA200);
             // Verify
-            assertThat(sut.getNum(itemA), is(2));
+            assertThat(sut.getNum(itemA100), is(2));
         }
     }
 }
