b09a42ca44b3e86e7b2dc414a7d44261364bff76
Yoshimasa Tanabe <tanabe.yoshimasa@nttcom.co.jp>
2012-11-27 12:37:12 +0900

    NumberUtils#isEvenを仮実装

---
+++ src/main/java/org/study/java/junit/exercises/basic18_4/NumberUtils.java
@@ -0,0 +1,9 @@
+package org.study.java.junit.exercises.basic18_4;
+
+public class NumberUtils {
+
+    public static boolean isEven(int x) {
+        return true;
+    }
+
+}
---
+++ src/test/java/org/study/java/junit/exercises/basic18_4/NumberUtilsTest.java
@@ -0,0 +1,14 @@
+package org.study.java.junit.exercises.basic18_4;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NumberUtilsTest {
+
+    @Test
+    public void isEven_10はtrueを返す() throws Exception {
+        assertThat(NumberUtils.isEven(10), is(true));
+    }
+}
