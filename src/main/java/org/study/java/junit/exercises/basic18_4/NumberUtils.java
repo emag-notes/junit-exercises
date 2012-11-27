aa9cfce3349541926f61fde11144bb0a62bc55f1
Yoshimasa Tanabe <tanabe.yoshimasa@nttcom.co.jp>
2012-11-27 14:07:21 +0900

    NumberUtils#isEvenを本実装

--- src/main/java/org/study/java/junit/exercises/basic18_4/NumberUtils.java
+++ src/main/java/org/study/java/junit/exercises/basic18_4/NumberUtils.java
@@ -2,8 +2,13 @@ package org.study.java.junit.exercises.basic18_4;
 
 public class NumberUtils {
 
+    /**
+     * 引数が2で割りきれる場合にtrueを返す
+     * @param x int型整数
+     * @return 引数が2で割りきれるかどうかの真偽値
+     */
     public static boolean isEven(int x) {
-        return true;
+        return x % 2 == 0;
     }
 
 }
--- src/test/java/org/study/java/junit/exercises/basic18_4/NumberUtilsTest.java
+++ src/test/java/org/study/java/junit/exercises/basic18_4/NumberUtilsTest.java
@@ -11,4 +11,9 @@ public class NumberUtilsTest {
     public void isEven_10はtrueを返す() throws Exception {
         assertThat(NumberUtils.isEven(10), is(true));
     }
+    
+    @Test
+    public void isEven_7はfalseを返す() throws Exception {
+        assertThat(NumberUtils.isEven(7), is(false));
+    }
 }
