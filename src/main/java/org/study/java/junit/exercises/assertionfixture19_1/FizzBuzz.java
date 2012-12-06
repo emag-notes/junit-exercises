e0965684244be220f51f15d477c885e247faae95
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-12-06 21:40:30 +0900

    FizzBuzz#createFizzBuzzListを本実装

--- src/main/java/org/study/java/junit/exercises/assertionfixture19_1/FizzBuzz.java
+++ src/main/java/org/study/java/junit/exercises/assertionfixture19_1/FizzBuzz.java
@@ -7,22 +7,17 @@ public class FizzBuzz {
 
     public static List<String> createFizzBuzzList(int num) {
         List<String> fizzBuzzList = new ArrayList<String>();
-        fizzBuzzList.add("1");
-        fizzBuzzList.add("2");
-        fizzBuzzList.add("Fizz");
-        fizzBuzzList.add("4");
-        fizzBuzzList.add("Buzz");
-        fizzBuzzList.add("Fizz");
-        fizzBuzzList.add("7");
-        fizzBuzzList.add("8");
-        fizzBuzzList.add("Fizz");
-        fizzBuzzList.add("Buzz");
-        fizzBuzzList.add("11");
-        fizzBuzzList.add("Fizz");
-        fizzBuzzList.add("13");
-        fizzBuzzList.add("14");
-        fizzBuzzList.add("FizzBuzz");
-        fizzBuzzList.add("16");
+        for (int i = 1; i <= num; i++) {
+            if (i % 15 == 0) {
+                fizzBuzzList.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                fizzBuzzList.add("Fizz");
+            } else if (i % 5 == 0) {
+                fizzBuzzList.add("Buzz");
+            } else {
+                fizzBuzzList.add(Integer.toString(i));
+            }
+        }
         return fizzBuzzList;
     }
 
