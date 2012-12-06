7cdcf3ad8d24fcb0fd8387b73e43ab95c3f0e0a2
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-12-07 08:08:45 +0900

    Employee用のカスタムMatcherを作成

--- src/test/java/org/study/java/junit/exercises/assertionfixture19_2/EmployeeTest.java
+++ src/test/java/org/study/java/junit/exercises/assertionfixture19_2/EmployeeTest.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.*;
 import java.io.InputStream;
 import java.util.List;
 
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class EmployeeTest {
@@ -23,5 +26,63 @@ public class EmployeeTest {
         assertThat(actualEmployee.getFirstName(), is("Ichiro"));
         assertThat(actualEmployee.getLastName(), is("Tanaka"));
         assertThat(actualEmployee.getEmail(), is("ichiro@example.com"));
+    }
+    
+    static class EmployeeMatcher extends BaseMatcher<Employee> {
+        
+        private final Employee expected;
+        private String field;
+        private Object expectedValue;
+        private Object actualValue;
+        
+        public static Matcher<Employee> employee(Employee expected) {
+            return new EmployeeMatcher(expected);
+        }
+        
+        EmployeeMatcher(Employee expected) {
+            this.expected = expected;
+        }
+        
+        @Override
+        public boolean matches(Object actual) {
+            if (expected == null) return (actual == null);
+            if (!(actual instanceof Employee)) return false;
+            Employee other = (Employee) actual;
+            if (notEquals(expected.getFirstName(), other.getFirstName())) {
+                field = "firstName";
+                expectedValue = expected.getFirstName();
+                actualValue   = other.getFirstName();
+                return false;
+            }
+            if (notEquals(expected.getLastName(), other.getLastName())) {
+                field = "lastName";
+                expectedValue = expected.getLastName();
+                actualValue   = other.getLastName();
+                return false;
+            }
+            if (notEquals(expected.getEmail(), other.getEmail())) {
+                field = "email";
+                expectedValue = expected.getEmail();
+                actualValue   = other.getEmail();
+                return false;
+            }
+            return true;
+        }
+
+        private boolean notEquals(Object obj, Object other) {
+            if (obj == null) return other != null;
+            return !obj.equals(other);
+        }
+
+        @Override
+        public void describeTo(Description desc) {
+            if (expected == null || field == null) {
+                desc.appendValue(expected);
+            } else {
+                desc.appendText(field + " is ").appendValue(expectedValue)
+                    .appendText(", but ").appendValue(actualValue);
+            }
+            
+        }
     }
 }
