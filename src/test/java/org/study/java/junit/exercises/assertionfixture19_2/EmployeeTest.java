15da38de2f3127ad2b85cebcd6d768010529d58b
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-12-08 17:38:37 +0900

    カスタムMatcherを利用したテストを作成

--- src/test/java/org/study/java/junit/exercises/assertionfixture19_2/EmployeeTest.java
+++ src/test/java/org/study/java/junit/exercises/assertionfixture19_2/EmployeeTest.java
@@ -2,13 +2,11 @@ package org.study.java.junit.exercises.assertionfixture19_2;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+import static org.study.java.junit.exercises.assertionfixture19_2.EmployeeMatcher.*;
 
 import java.io.InputStream;
 import java.util.List;
 
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class EmployeeTest {
@@ -28,61 +26,17 @@ public class EmployeeTest {
         assertThat(actualEmployee.getEmail(), is("ichiro@example.com"));
     }
     
-    static class EmployeeMatcher extends BaseMatcher<Employee> {
-        
-        private final Employee expected;
-        private String field;
-        private Object expectedValue;
-        private Object actualValue;
-        
-        public static Matcher<Employee> employee(Employee expected) {
-            return new EmployeeMatcher(expected);
-        }
-        
-        EmployeeMatcher(Employee expected) {
-            this.expected = expected;
-        }
-        
-        @Override
-        public boolean matches(Object actual) {
-            if (expected == null) return (actual == null);
-            if (!(actual instanceof Employee)) return false;
-            Employee other = (Employee) actual;
-            if (notEquals(expected.getFirstName(), other.getFirstName())) {
-                field = "firstName";
-                expectedValue = expected.getFirstName();
-                actualValue   = other.getFirstName();
-                return false;
-            }
-            if (notEquals(expected.getLastName(), other.getLastName())) {
-                field = "lastName";
-                expectedValue = expected.getLastName();
-                actualValue   = other.getLastName();
-                return false;
-            }
-            if (notEquals(expected.getEmail(), other.getEmail())) {
-                field = "email";
-                expectedValue = expected.getEmail();
-                actualValue   = other.getEmail();
-                return false;
-            }
-            return true;
-        }
-
-        private boolean notEquals(Object obj, Object other) {
-            if (obj == null) return other != null;
-            return !obj.equals(other);
-        }
-
-        @Override
-        public void describeTo(Description desc) {
-            if (expected == null || field == null) {
-                desc.appendValue(expected);
-            } else {
-                desc.appendText(field + " is ").appendValue(expectedValue)
-                    .appendText(", but ").appendValue(actualValue);
-            }
-            
-        }
+    @Test
+    public void loadでEmployeeの一覧を取得できる_カスタムMatcher利用() throws Exception {
+        // SetUp
+        InputStream input = getClass().getResourceAsStream("Employee.txt");
+        Employee expected = new Employee("Ichiro", "Tanaka", "ichiro@example.com");
+        // Exercise
+        List<Employee> actual = Employee.load(input);
+        // Verify
+        assertThat(actual, is(notNullValue()));
+        assertThat(actual.size(), is(1));
+        assertThat(actual.get(0), is(employee(expected)));
     }
+    
 }
