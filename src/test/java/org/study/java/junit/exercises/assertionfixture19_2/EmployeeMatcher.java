947aa6c013b48fe0ca7c729a92a5bec431a652df
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-12-08 18:05:17 +0900

    EmployeeMatcherを別クラスにした

---
+++ src/test/java/org/study/java/junit/exercises/assertionfixture19_2/EmployeeMatcher.java
@@ -0,0 +1,64 @@
+package org.study.java.junit.exercises.assertionfixture19_2;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+public class EmployeeMatcher extends BaseMatcher<Employee> {
+
+    private final Employee expected;
+    private String field;
+    private Object expectedValue;
+    private Object actualValue;
+    
+    public static Matcher<Employee> employee(Employee expected) {
+        return new EmployeeMatcher(expected);
+    }
+    
+    EmployeeMatcher(Employee expected) {
+        this.expected = expected;
+    }
+    
+    @Override
+    public boolean matches(Object actual) {
+        if (expected == null) return (actual == null);
+        if (!(actual instanceof Employee)) return false;
+        Employee other = (Employee) actual;
+        if (notEquals(expected.getFirstName(), other.getFirstName())) {
+            field = "firstName";
+            expectedValue = expected.getFirstName();
+            actualValue   = other.getFirstName();
+            return false;
+        }
+        if (notEquals(expected.getLastName(), other.getLastName())) {
+            field = "lastName";
+            expectedValue = expected.getLastName();
+            actualValue   = other.getLastName();
+            return false;
+        }
+        if (notEquals(expected.getEmail(), other.getEmail())) {
+            field = "email";
+            expectedValue = expected.getEmail();
+            actualValue   = other.getEmail();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean notEquals(Object obj, Object other) {
+        if (obj == null) return other != null;
+        return !obj.equals(other);
+    }
+
+    @Override
+    public void describeTo(Description desc) {
+        if (expected == null || field == null) {
+            desc.appendValue(expected);
+        } else {
+            desc.appendText(field + " is ").appendValue(expectedValue)
+                .appendText(", but ").appendValue(actualValue);
+        }
+        
+    }
+    
+}
