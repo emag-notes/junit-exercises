1f88597ad097afd7bd8ae0806119c414d0b7514e
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-12-11 07:44:46 +0900

    境界値テストの構造化とパラメータ化を実施

--- src/test/java/org/study/java/junit/exercises/assertionfixture19_4/RangeTest.java
+++ src/test/java/org/study/java/junit/exercises/assertionfixture19_4/RangeTest.java
@@ -6,43 +6,96 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
 @RunWith(Enclosed.class)
 public class RangeTest {
 
-    public static class minが0_0でmaxが10_5のとき {
+    @RunWith(Enclosed.class)
+    public static class minが0_0でmaxが10_5で {
         
-        Range sut;
-        
-        @Before
-        public void setUp() throws Exception {
-            sut = new Range(0.0, 10.5);
+        @RunWith(Theories.class)
+        public static class かつ範囲外の場合 {
+            
+            Range sut;
+            
+            @Before
+            public void setUp() throws Exception {
+                sut = new Range(0.0, 10.5);
+            }
+            
+            @DataPoints
+            public static double[] VALUES = {-0.1, 10.6};
+            
+            @Theory
+            public void containsはfalseを返す(double value) throws Exception {
+                assertThat("value=" + value, sut.contains(value), is(false));
+            }
         }
         
-        @Test
-        public void contains_miunus0_1はfalse() throws Exception {
-            assertThat(sut.contains(-0.1), is(false));
-        }
-        
-        @Test
-        public void contains_0_0はtrue() throws Exception {
-            assertThat(sut.contains(0.0), is(true));
-        }
-        
-        @Test
-        public void contains_10_5はtrue() throws Exception {
-            assertThat(sut.contains(10.5), is(true));
-        }
-        
-        @Test
-        public void contains_10_6はfalse() throws Exception {
-            assertThat(sut.contains(10.6), is(false));
+        @RunWith(Theories.class)
+        public static class かつ範囲の場合 {
+            
+            Range sut;
+            
+            @Before
+            public void setUp() throws Exception {
+                sut = new Range(0.0, 10.5);
+            }
+            
+            @DataPoints
+            public static double[] VALUES = {0.0, 10.5};
+            
+            @Theory
+            public void containsはtrueを返す(double value) throws Exception {
+                assertThat("value=" + value, sut.contains(value), is(true));
+            }
         }
     }
     
-    public static class minがminus5_1でmaxが5_1のとき {
+    @RunWith(Enclosed.class)
+    public static class minがminus5_1でmaxが5_1で {
         
+        @RunWith(Theories.class)
+        public static class かつ範囲外の場合 {
+            
+            Range sut;
+            
+            @Before
+            public void setUp() throws Exception {
+                sut = new Range(-5.1, 5.1);
+            }
+            
+            @DataPoints
+            public static double[] VALUES = {-5.2, 5.2};
+            
+            @Theory
+            public void containsはfalseを返す(double value) throws Exception {
+                assertThat("value=" + value, sut.contains(value), is(false));
+            }
+        }
+        
+        @RunWith(Theories.class)
+        public static class かつ範囲の場合 {
+            
+            Range sut;
+            
+            @Before
+            public void setUp() throws Exception {
+                sut = new Range(-5.1, 5.1);
+            }
+            
+            @DataPoints
+            public static double[] VALUES = {-5.1, 5.1};
+            
+            @Theory
+            public void containsはtrueを返す(double value) throws Exception {
+                assertThat("value=" + value, sut.contains(value), is(true));
+            }
+        }
         Range sut;
         
         @Before
