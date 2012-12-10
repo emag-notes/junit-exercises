756f415e345e5cb55cc6658f2f4d987e2f8f43ea
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-12-11 07:09:37 +0900

    Rangeクラスを新規作成

---
+++ src/main/java/org/study/java/junit/exercises/assertionfixture19_4/Range.java
@@ -0,0 +1,13 @@
+package org.study.java.junit.exercises.assertionfixture19_4;
+
+public class Range {
+
+    public final double min_;
+    public final double max_;
+    
+    public Range(double min, double max) {
+        min_ = min;
+        max_ = max;
+    }
+    
+}
