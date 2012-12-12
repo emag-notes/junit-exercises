3fdd82ba1ffb40233965f022c87c3ab2df91dbbc
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-12-13 07:22:55 +0900

    ConsumptionTax#applyを仮実装

---
+++ src/test/java/org/study/java/junit/exercises/assertionfixture19_5/ConsumptionTax.java
@@ -0,0 +1,13 @@
+package org.study.java.junit.exercises.assertionfixture19_5;
+
+public class ConsumptionTax {
+
+    public ConsumptionTax(int taxRate) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public Integer apply(int price) {
+        return 105;
+    }
+
+}
---
+++ src/test/java/org/study/java/junit/exercises/assertionfixture19_5/ConsumptionTaxTest.java
@@ -0,0 +1,41 @@
+package org.study.java.junit.exercises.assertionfixture19_5;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+@RunWith(Theories.class)
+public class ConsumptionTaxTest {
+
+    @DataPoints
+    public static Fixture[] FIXTURES = new Fixture[] {
+        new Fixture( 5,  100,  105),
+        new Fixture( 5, 3000, 3150),
+        new Fixture(10,   50,   55),
+        new Fixture( 5,   50,   52),
+        new Fixture( 3,   50,   51),
+    };
+    
+    @Theory
+    public void applyで消費税が加算された価格が取得できる(Fixture fixture) throws Exception {
+        ConsumptionTax sut = new ConsumptionTax(fixture.taxRate);
+        String desc = "when rate=" + fixture.taxRate + "," +
+                      "price= "    + fixture.price;
+        assertThat(desc, sut.apply(fixture.price), is(fixture.expected));
+    }
+}
+
+class Fixture {
+    final int taxRate;
+    final int price;
+    final int expected;
+    Fixture(int taxRage, int price, int expected) {
+        this.taxRate  = taxRage;
+        this.price    = price;
+        this.expected = expected;
+    }
+}
