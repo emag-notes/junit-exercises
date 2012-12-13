f16a956d499fd393fecee618caffb96dd4c8d84d
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-12-14 07:45:14 +0900

    Frameworks#isSupportの本実装

--- src/main/java/org/study/java/junit/exercises/assertionfixture19_6/Frameworks.java
+++ src/main/java/org/study/java/junit/exercises/assertionfixture19_6/Frameworks.java
@@ -3,7 +3,20 @@ package org.study.java.junit.exercises.assertionfixture19_6;
 public class Frameworks {
 
     public static boolean isSupport(ApplicationServer appServer, Database db) {
-        return true;
+        
+        switch (appServer) {
+        case GlassFish:
+            return true;
+            
+        case JBoss:
+            return db == Database.DB2 || db == Database.PostgreSQL;
+            
+        case Tomcat:
+            return db == Database.MySQL;
+        default:
+            return false;
+        }
+        
     }
 
 }
