40c5674fee0fac24f91e2a6e695e798eac571aa3
emag <emag@mail.emamotor.org>
2013-01-29 08:10:16 +0900

    LogAnalyzer のテストを追加

---
+++ src/test/java/org/study/java/junit/exercises/testdouble20_2/LogAnalyzerTest.java
@@ -0,0 +1,51 @@
+package org.study.java.junit.exercises.testdouble20_2;
+
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class LogAnalyzerTest {
+
+    @Rule
+    public ExpectedException ex = ExpectedException.none();
+    
+    @Test
+    public void LogLoaderが例外を送出するときAnalyzeExceptionが再送出される() throws Exception {
+        
+        // SetUp
+        LogAnalyzer sut = new LogAnalyzer();
+        final IOException errorCause = new IOException("error by stub");
+        LogLoader mockLoader = mock(LogLoader.class);
+        sut.logLoader = mockLoader;
+        when(mockLoader.load("test")).thenThrow(errorCause);
+        ex.expect(LogAnalyzer.AnalyzeException.class);
+        ex.expectMessage("error by stub");
+        ex.expect(new BaseMatcher<Object>() {
+            
+            Throwable cause;
+            
+            @Override
+            public boolean matches(Object item) {
+                Throwable t = (Throwable) item;
+                cause = t.getCause();
+                return cause == errorCause;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendValue(cause);
+            }
+            
+        });
+        
+        // Exercise
+        sut.analyze("test");
+        
+    }
+}
