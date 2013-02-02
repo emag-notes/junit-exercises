c63d4a4bc1abe86e0c9e2e8f01df91ee896bba4b
emag <emag@mail.emamotor.org>
2013-02-03 06:03:55 +0900

    20.3 外部システムに依存するテストを作成

---
+++ src/main/java/org/study/java/junit/exercises/testdouble20_3/NetworkLoader.java
@@ -0,0 +1,12 @@
+package org.study.java.junit.exercises.testdouble20_3;
+
+import java.io.InputStream;
+
+public class NetworkLoader {
+
+    public InputStream getInput() {
+        // TODO 未実装
+        return null;
+    }
+
+}
---
+++ src/test/java/org/study/java/junit/exercises/testdouble20_3/NetworkResourcesTest.java
@@ -0,0 +1,27 @@
+package org.study.java.junit.exercises.testdouble20_3;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.Test;
+
+public class NetworkResourcesTest {
+
+    @Test
+    public void loadでネットワークから取得した文字列を返す() throws Exception {
+        // SetUp
+        String expected = "Hello World";
+        NetworkLoader mockLoader = mock(NetworkLoader.class);
+        ByteArrayInputStream input = new ByteArrayInputStream(expected.getBytes());
+        when(mockLoader.getInput()).thenReturn(input);
+        NetworkResources sut = new NetworkResources();
+        sut.loader = mockLoader;
+        // Exercise
+        String actual = sut.load();
+        // Verify
+        assertThat(actual, is(expected));
+    }
+}
