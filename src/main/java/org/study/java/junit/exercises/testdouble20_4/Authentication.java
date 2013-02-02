b71c741c3e67f9a95f135b4e0941ecada39405ac
emag <emag@mail.emamotor.org>
2013-02-03 07:27:29 +0900

    Authentication#authenticateの本実装

--- src/main/java/org/study/java/junit/exercises/testdouble20_4/Authentication.java
+++ src/main/java/org/study/java/junit/exercises/testdouble20_4/Authentication.java
@@ -2,11 +2,16 @@ package org.study.java.junit.exercises.testdouble20_4;
 
 public class Authentication {
 
-    public AccountDao dao;
+    AccountDao dao;
 
     public Account authenticate(String userId, String password) {
-        // TODO Auto-generated method stub
-        return null;
+        assert dao != null;
+        
+        Account account = dao.findOrNull(userId);
+        
+        if (account == null) return null;
+        
+        return account.getPassword().equals(password) ? account : null;
     }
 
 }
--- src/test/java/org/study/java/junit/exercises/testdouble20_4/AuthenticationTest.java
+++ src/test/java/org/study/java/junit/exercises/testdouble20_4/AuthenticationTest.java
@@ -2,12 +2,12 @@ package org.study.java.junit.exercises.testdouble20_4;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
-import static org.mockito.Mockito.*;
 
 @RunWith(Enclosed.class)
 public class AuthenticationTest {
@@ -28,4 +28,39 @@ public class AuthenticationTest {
         }
     }
     
+    public static class アカウントが存在しパスワードが一致する場合 {
+        Authentication sut;
+        Account account;
+        
+        @Before
+        public void setUp() throws Exception {
+            sut = new Authentication();
+            sut.dao = mock(AccountDao.class);
+            account = new Account("user001", "pw123");
+            when(sut.dao.findOrNull("user001")).thenReturn(account);
+        }
+        
+        @Test
+        public void authenticateはaccountを返す() throws Exception {
+            assertThat(sut.authenticate("user001", "pw123"), is(account));
+        }
+    }
+    
+    public static class アカウントが存在しパスワードが一致しない場合 {
+        Authentication sut;
+        Account account;
+        
+        @Before
+        public void setUp() throws Exception {
+            sut = new Authentication();
+            sut.dao = mock(AccountDao.class);
+            account = new Account("user001", "pw999");
+            when(sut.dao.findOrNull("user001")).thenReturn(account);
+        }
+        
+        @Test
+        public void authenticateはnullを返す() throws Exception {
+            assertThat(sut.authenticate("user001", "pw123"), is(nullValue()));
+        }
+    }
 }
